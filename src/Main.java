f399e1b27e11a630f0c10f48bf924dcd1bc13dad
Nate <nwyche9@gmail.com>
2019-04-09 23:00:08 -0400

    Add files via upload
    Added doubly linked list implementation in Java.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,26 @@
 public class Main {
-
-    public static void main(String[] args){//bare hashtable
-
-        Employee tchalla = new Employee("Tchalla", "King", 000);
-        Employee tony = new Employee("Tony", "Stark", 002);
+    public static void main(String[] args){
+        Employee tchalla = new Employee("Tchalla","King",001);
+        Employee stark = new Employee("Tony", "Stark", 002);
         Employee bias = new Employee("Offensive", "Bias", 003);
         Employee bruce = new Employee("Bruce", "Banner", 004);
-        Employee end = new Employee("Mike", "End", 005);
 
-        Hashtables ht = new Hashtables();
+        DblLinkedList lst = new DblLinkedList();
 
-        ht.put("Tchalla",tchalla);
-        ht.put("Tony", tony);
-        ht.put("Bruce", bruce);
-        ht.put("Bias", bias);
-        ht.put("Mike", end);
+        lst.addToFront(tchalla);
+        lst.addToFront(stark);
+        lst.addToFront(bias);
+        lst.addToFront(bruce);
 
-        //ht.printTable();
-
-        System.out.println("Retrieve Tony: " + ht.get("Tony"));
-        System.out.println("Retrieve Bias: " + ht.get("Bias"));
-
-        ht.remove("Mike");
-        ht.remove("Bias");
-        ht.printTable();
-
+        Employee cap = new Employee("Captain","America",005);
+        lst.addToTail(cap);
+        lst.printList();
+        lst.addBefore(new Employee("Admiral","Thrawn",010), bias);
+        System.out.println(lst.getSize());
+        lst.removeFromFront();
+        lst.printList();
+        lst.removeFromEnd();
+        lst.printList();
+        System.out.println(lst.getSize());
     }
 }
